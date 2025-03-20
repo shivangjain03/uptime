3dd8ce15e47aa020c2a05d9ffbdf599ad97d2cf2
shivangjain03 <shivangjain2005@gmail.com>
2025-03-20 12:37:28 -0700

    Middleware setup done

--- dpin-uptime/apps/api/index.ts
+++ dpin-uptime/apps/api/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import { authMiddleware } from './middleware';
+import {prismaClient} from "db/client";
+
+const app = express();
+app.use(express.json());
+app.post('/api/v1/website', authMiddleware, async (req, res) => {
+    const userId = req.userId!;
+    const url = req.body.url;
+    const data = await prismaClient.website.create({
+        data: {
+            userId,
+            url
+        }
+    })
+
+    res.json({
+        id: data.id
+    })
+});
+
+app.get('/api/v1/status', authMiddleware, (req, res) => {
+});
+
+app.get('/api/v1/websites', authMiddleware, (req, res) => {
+});
+
+app.delete('/api/v1/website/', authMiddleware, (req, res) => {
+});
+
+app.listen(3000);
